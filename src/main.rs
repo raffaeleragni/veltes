6c627be06acc3b0b2c4747cc9c86d9b17b146c0d
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-03-15 18:39:10 +0100

    allow database in any order

--- src/main.rs
+++ src/main.rs
@@ -80,15 +80,15 @@ struct Statics;
 
 #[tokio::main]
 async fn main() {
-    let app = App::new();
+    let db = database().await;
+    sqlx::migrate!().run(&db).await.unwrap();
     let r = Router::new()
         .route("/", get(index))
         .route("/ui/samples", get(get_all_samples))
         .route("/ui/sample", post(add_new_sample))
         .route("/ui/sample/:id", get(get_one_sample));
-    let db = database().await;
-    sqlx::migrate!().run(&db).await.unwrap();
-    app.router(r)
+    App::new()
+        .router(r)
         .inject(db)
         .statics::<Statics>()
         .start()
