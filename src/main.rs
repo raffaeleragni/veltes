f4edb4525044c7a11d5c51db6036cbb9407712f0
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-09-17 21:40:42 +0200

    use AppResult also on main

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ mod ui;
 use velvet_web::prelude::*;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> AppResult<()> {
     #[derive(RustEmbed)]
     #[folder = "statics"]
     struct S;
 
-    JWT::JwkUrls.setup().await.unwrap();
+    JWT::JwkUrls.setup().await?;
 
     let db = postgres().await;
-    sqlx::migrate!().run(&db).await.unwrap();
+    sqlx::migrate!().run(&db).await?;
 
     App::new()
         .router(ui::app())
@@ -24,4 +24,5 @@ async fn main() {
         .statics::<S>()
         .start()
         .await;
+    Ok(())
 }
